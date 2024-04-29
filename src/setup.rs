cdfad0e6743197d89b8736fb47c8e47e0bb30b26
Dmitry <volikovd17@gmail.com>
2024-04-29 12:08:32 +0300

    [Egline server] Fixed cargon run db connection

--- src/setup.rs
+++ src/setup.rs
@@ -1,31 +1,12 @@
-use sea_orm::*;
+use sea_orm::{Database, DbErr, DatabaseConnection};
 
 // Replace with your database URL and database name
 const DATABASE_URL: &str = "postgres://admin:postgres@localhost:5432";
 const DB_NAME: &str = "egline";
 
 pub(super) async fn set_up_db() -> Result<DatabaseConnection, DbErr> {
-    let db = Database::connect(DATABASE_URL).await?;
-
-    let db = match db.get_database_backend() {
-        DbBackend::MySql => {
-            db.execute(Statement::from_string(
-                db.get_database_backend(),
-                format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
-            ))
-            .await?;
-
-            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-            Database::connect(&url).await?
-        }
-        
-        DbBackend::Postgres => {
-            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-            Database::connect(&url).await?
-        }
-
-        DbBackend::Sqlite => db,
-    };
+    let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+    let db = Database::connect(&url).await?;
 
     Ok(db)
 }
