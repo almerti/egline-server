24f24071c8652054a2ae90dd950a294d68deb1d0
Dmitry <volikovd17@gmail.com>
2024-05-16 22:54:30 +0300

    Get chapter text and audio data

--- src/routes/chapter_route.rs
+++ src/routes/chapter_route.rs
@@ -1,12 +1,16 @@
+use std::fs::File;
+use std::io::Read;
+
 use chrono::NaiveDate;
 use rocket::serde::json::Json;
+use rocket::tokio::fs;
 use rocket::State;
 
 use rocket::response::status;
 use rocket::http::Status;
 
 use crate::entities::prelude::Chapter;
-use crate::entities::chapter::{ActiveModel, Model, Column};
+use crate::entities::chapter::{self, ActiveModel, Column, Model};
 use sea_orm::{prelude::DbErr, ActiveModelTrait, ActiveValue, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 
 #[get("/")]
@@ -82,18 +86,26 @@ async fn create_chapter(
         )),
         Ok(None) => {
             let filepath = format!("/{}/{}/", chapter_data.book_id, chapter_data.number);
+            let chapter_dir = fs::create_dir(
+                format!("storage{}", filepath)
+            ).await;
 
-            let chapter:Result<Model, DbErr> = ActiveModel {
-                book_id: ActiveValue::set(chapter_data.book_id.clone()),
-                title: ActiveValue::set(chapter_data.title.clone()),
-                filepath: ActiveValue::set(filepath),
-                number: ActiveValue::set(chapter_data.number.clone()),
-                date: ActiveValue::set(chapter_data.date),
-                ..Default::default()
-            }.insert(db).await;
+            match chapter_dir {
+                Ok(_) => {
+                    let chapter:Result<Model, DbErr> = ActiveModel {
+                        book_id: ActiveValue::set(chapter_data.book_id.clone()),
+                        title: ActiveValue::set(chapter_data.title.clone()),
+                        filepath: ActiveValue::set(filepath),
+                        number: ActiveValue::set(chapter_data.number.clone()),
+                        date: ActiveValue::set(chapter_data.date),
+                        ..Default::default()
+                    }.insert(db).await;
 
-            match chapter {
-                Ok(_) => Ok(Json(format!("Chapter {} was successfully created", chapter_data.title.clone()))),
+                    match chapter {
+                        Ok(_) => Ok(Json(format!("Chapter {} was successfully created", chapter_data.title.clone()))),
+                        Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
+                    }  
+                },
                 Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
             }
         },
@@ -109,32 +121,32 @@ async fn update_chapter(
 ) -> Result<Json<String>, status::Custom<String>> {
     let db: &DatabaseConnection = db as &DatabaseConnection;
     let is_chapter_exists = Chapter::find()
-        .filter(Column::BookId.eq(chapter_data.book_id))
-        .filter(Column::Number.eq(chapter_data.number))
-        .one(db)
-        .await;
+        .filter(Column::Id.eq(id))
+        .all(db)
+        .await
+        .unwrap();
 
-    match is_chapter_exists {
-        Ok(Some(chapter)) => Err(status::Custom(
+    if is_chapter_exists[0].number != chapter_data.number {
+        return Err(status::Custom(
             Status::InternalServerError,
-            format!("Book {} has chapter with number {}", chapter.book_id, chapter.number)
-        )),
-        Ok(None) => {
-            let updated_chapter:Result<Model, DbErr> = ActiveModel {
-                id: ActiveValue::set(id),
-                book_id: ActiveValue::set(chapter_data.book_id.clone()),
-                title: ActiveValue::set(chapter_data.title.clone()),
-                filepath: ActiveValue::set(chapter_data.filepath.clone()),
-                number: ActiveValue::set(chapter_data.number.clone()),
-                date: ActiveValue::set(chapter_data.date),
-                ..Default::default()
-            }.insert(db).await;
+            format!("Book {} has chapter with number {}", is_chapter_exists[0].book_id, chapter_data.number)
+        ))
+    }
 
-            match updated_chapter {
-                Ok(_) => Ok(Json(format!("Chapter {} was successfully updated", chapter_data.title.clone()))),
-                Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
-            }
-        },
+    let filepath = format!("/{}/{}/", chapter_data.book_id, chapter_data.number);
+
+    let updated_chapter:Result<Model, DbErr> = ActiveModel {
+        id: ActiveValue::set(id),
+        book_id: ActiveValue::set(chapter_data.book_id.clone()),
+        title: ActiveValue::set(chapter_data.title.clone()),
+        filepath: ActiveValue::set(filepath),
+        number: ActiveValue::set(chapter_data.number.clone()),
+        date: ActiveValue::set(chapter_data.date),
+        ..Default::default()
+    }.update(db).await;
+
+    match updated_chapter {
+        Ok(_) => Ok(Json(format!("Chapter {} was successfully updated", chapter_data.title.clone()))),
         Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
     }
 }
@@ -146,17 +158,97 @@ async fn delete_chapter(
 ) -> Result<Json<String>, status::Custom<String>> {
     let db: &DatabaseConnection = db as &DatabaseConnection;
 
+    let delete_chapter = Chapter::find()
+        .filter(Column::Id.eq(id))
+        .all(db)
+        .await
+        .unwrap();
+
     let deleted_chapter = ActiveModel {
         id: ActiveValue::set(id),
         ..Default::default()
     }.delete(db).await;
 
     match deleted_chapter {
-        Ok(result) => Ok(Json(format!("Number of deleted entries: {}", result.rows_affected))),
+        Ok(result) => {
+            let filepath = format!("/{}/{}/", delete_chapter[0].book_id, delete_chapter[0].number);
+            let chapter_dir = fs::remove_dir(
+                format!("storage{}", filepath)
+            ).await;
+
+            match chapter_dir {
+                Ok(_) => Ok(Json(format!("Number of deleted entries: {}", result.rows_affected))),
+                Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
+            }
+        },
         Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
     }
 }
 
+#[get("/text/<chapter_id>")]
+async fn get_chapter_text(
+    db: &State<DatabaseConnection>,
+    chapter_id: i32
+) -> Result<String, status::Custom<String>> {
+    let db: &DatabaseConnection = db as &DatabaseConnection;
+    let chapter_data = Chapter::find()
+        .filter(Column::Id.eq(chapter_id))
+        .all(db)
+        .await
+        .unwrap();
+
+    if chapter_data.len() == 0 {
+        return Err(status::Custom(Status::NotFound, format!("No chapter with id {}", chapter_id)))
+    }
+
+    let filepath = format!("storage{}/text.txt", chapter_data[0].filepath);
+    let file = File::options().read(true).open(filepath);
+
+    match file {
+        Ok(mut result) => {
+            let mut content = String::new();
+            let _ = result.read_to_string(&mut content);
+
+            Ok(content)
+        },
+        Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
+    }
+}
+
+#[get("/audio/<chapter_id>")]
+async fn get_chapter_audio(
+    db: &State<DatabaseConnection>,
+    chapter_id: i32
+) -> Result<Vec<u8>, status::Custom<String>> {
+    let db: &DatabaseConnection = db as &DatabaseConnection;
+    let chapter_data = Chapter::find()
+        .filter(Column::Id.eq(chapter_id))
+        .all(db)
+        .await
+        .unwrap();
+
+    if chapter_data.len() == 0 {
+        return Err(status::Custom(Status::NotFound, format!("No chapter with id {}", chapter_id)))
+    }
+
+    let filepath = format!("storage{}/audio.mp3", chapter_data[0].filepath);
+    let mut file = File::options().read(true).open(filepath.clone()).expect("Can not open file");
+    let metadata = fs::metadata(filepath.clone()).await.expect("Can not read metadata");
+    let mut buf = vec![0; metadata.len() as usize];
+    let _ = file.read(&mut buf).expect("Buffer overflow");
+
+    Ok(buf)
+}
+
 pub fn get_all_chapter_methods() -> Vec<rocket::Route> {
-    routes![get_all_chapters, get_chapter_by_id, create_chapter, update_chapter, delete_chapter, get_book_chapters]
+    routes![
+        get_all_chapters,
+        get_chapter_by_id,
+        create_chapter,
+        update_chapter,
+        delete_chapter,
+        get_book_chapters,
+        get_chapter_text,
+        get_chapter_audio
+    ]
 }
